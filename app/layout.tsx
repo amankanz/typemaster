e4782ac439ab67c7c3aec8a47d29e9facf2d09e4
Amani Kaneza Fred <95358548+amankanz@users.noreply.github.com>
2025-05-21 17:15:12 +0300

    ft: Footer component completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { sora } from "@/libs/fonts";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "TypeMaster – Keyboard Speed Game",
@@ -64,6 +65,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
